24342e77358204afe38bb35c172bf947f71c9eb9
Pri1esh <pritesh.lodha@advaiya.com>
2024-02-08 19:15:46 +0530

    "leadership"

--- src/pages/leadership/leadership.tsx
+++ src/pages/leadership/leadership.tsx
@@ -3,23 +3,37 @@ import Banner from '../../shared/banner/banner';
 import graybox from "../../assets/images/graybox.png"
 import bannerImg from "../../assets/images/leadership.png";
 
+import vinay from "../../assets/images/vinay.png"
+import jeet from "../../assets/images/jeet.png"
+import birva from "../../assets/images/birva.png"
+import sunipa from "../../assets/images/sunipa.png"
+import mohan from "../../assets/images/mohan.png"
+import dinesh from "../../assets/images/dinesh.png"
+
+import av from "../../assets/images/av.png"
+import deepak from "../../assets/images/deepak.png"
+import amitabh from "../../assets/images/amitabh.png"
+import manish from "../../assets/images/manish.png"
+import srinivas from "../../assets/images/srinivas.png"
+import kaniya from "../../assets/images/kanaiya.png"
+import satish from "../../assets/images/satish.png"
 
 const Leadership = () => {
 
     const [LeaderNo,setLeaderNo] = useState<any>();
     const leaders = [
-        {src:graybox,name:"Karan Adani",designation:"CEO, Adani Ports & SEZ Ltd"},
-        {src:graybox,name:"Karan Adani",designation:"CEO, Adani Ports & SEZ Ltd"},
-        {src:graybox,name:"Karan Adani",designation:"CEO, Adani Ports & SEZ Ltd"},
-        {src:graybox,name:"Karan Adani",designation:"CEO, Adani Ports & SEZ Ltd"},
-        {src:graybox,name:"Karan Adani",designation:"CEO, Adani Ports & SEZ Ltd"},
-        {src:graybox,name:"Karan Adani",designation:"CEO, Adani Ports & SEZ Ltd"},
-        {src:graybox,name:"Karan Adani",designation:"CEO, Adani Ports & SEZ Ltd"},
-        {src:graybox,name:"Karan Adani",designation:"CEO, Adani Ports & SEZ Ltd"},
-        {src:graybox,name:"Karan Adani",designation:"CEO, Adani Ports & SEZ Ltd"},
-        {src:graybox,name:"Karan Adani",designation:"CEO, Adani Ports & SEZ Ltd"},
-        {src:graybox,name:"Karan Adani",designation:"CEO, Adani Ports & SEZ Ltd"},
-        {src:graybox,name:"Karan Adani",designation:"CEO, Adani Ports & SEZ Ltd"},
+        {src:vinay,name:"Vinay Prakash",designation:"Managing Director"},
+        {src:jeet,name:"Jeet Gautam Adani",designation:"Additional Director"},
+        {src:birva,name:"Birva Chiragbhai Patel",designation:"Director"},
+        {src:sunipa,name:"Sunipa Roy",designation:"Business Head Copper"},
+        {src:mohan,name:"Mohanasundaram Natarajan",designation:"Business Head Strategic & Precious Metals"},
+        {src:dinesh,name:"Dinesh Kumar Jain",designation:"Dinesh Kumar Jain"},
+        {src:av,name:"AV Krishnan",designation:"Head Raw Material Sourcing & Marketing "},
+        {src:amitabh,name:"Amitabh Mishra",designation:"CHRO Natural Resources"},
+        {src:manish,name:"Manish Saxena",designation:"CEO, Adani Ports & SEZ Ltd"},
+        {src:srinivas,name:"Srinivas V Gutta",designation:"Chief Digital Officer Natural Resources"},
+        {src:kaniya,name:"Kanaiya Thakker",designation:"Head Legal Natural Resources "},
+        {src:satish,name:"Satish Sinha",designation:"Head Sustainability & ESG "},
     ];
     useEffect(()=>{
         window.innerWidth <= 768 ? setLeaderNo(4) : setLeaderNo(leaders.length);
